084706b16699159df35ab1a683feaa362fc4da9a
valebuyondo <buyondovale@gmail.com>
2023-07-22 15:02:53 -0700

    add

--- printf.c
+++ printf.c
@@ -4,11 +4,14 @@
 int _printf(const char *format, ...)
 {
     va_list arg_list;
+    int chars_printed = 0;
+    int i, j;
+    char c;
+    char *str;
+
     va_start(arg_list, format);
 
-    int chars_printed = 0;
-
-    for (int i = 0; format[i] != '\0'; i++)
+    for (i = 0; format[i] != '\0'; i++)
     {
         if (format[i] == '%')
         {
@@ -18,14 +21,14 @@ int _printf(const char *format, ...)
 
             if (format[i] == 'c')
             {
-                char c = va_arg(arg_list, int);
+                c = va_arg(arg_list, int);
                 putchar(c);
                 chars_printed++;
             }
             else if (format[i] == 's')
             {
-                char *str = va_arg(arg_list, char *);
-                for (int j = 0; str[j] != '\0'; j++)
+                str = va_arg(arg_list, char *);
+                for (j = 0; str[j] != '\0'; j++)
                 {
                     putchar(str[j]);
                     chars_printed++;
