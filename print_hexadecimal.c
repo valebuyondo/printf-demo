5ae2d0a1c0a0323bc853a68ab901214b4f55e8f2
valebuyondo <42058151+valebuyondo@users.noreply.github.com>
2023-07-25 12:11:06 +0300

    Update print_hexadecimal.c

--- print_hexadecimal.c
+++ print_hexadecimal.c
@@ -34,3 +34,18 @@ int print_HEX(va_list arg)
 {
 	return (print_hex_base(arg, 'A'));
 }
+/**
+* print_binary - function that prints the binary representation of a number
+* @n: number to be printed in binary
+* @printed: hold the number of characters printed
+*/
+
+void print_binary(unsigned int n, unsigned int *printed)
+{
+	if (n > 1)
+	{
+		*printed += 1;
+		print_binary(n >> 1, printed);
+	}
+	_putchar((n & 1) + '0');
+}
