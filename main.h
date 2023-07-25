b401771c27d25e3b50dc2e0e2d7cd4417230f2fc
valebuyondo <42058151+valebuyondo@users.noreply.github.com>
2023-07-25 11:51:10 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,20 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <limits.h>
-#include <unistd.h>
-
-
-
 /**
  * struct format - match the conversion specifiers for printf
  * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
  * @f: type pointer to function for the conversion specifier
  *
  */
+typedef struct identifierStruct
+{
+	char *indentifier;
+
+	int (*printer)(va_list);
+} identifierStruct;
+
 
 /**typedef struct format
 {
@@ -22,36 +21,22 @@
 	int (*f)();
 } convert_match;
 **/
-int printf_pointer(va_list val);
-int printf_hex_aux(unsigned long int num);
-int printf_HEX_aux(unsigned int num);
-int printf_exclusive_string(va_list val);
-int printf_HEX(va_list val);
-int printf_hex(va_list val);
-int printf_oct(va_list val);
-int printf_unsigned(va_list args);
-int printf_bin(va_list val);
-int printf_srev(va_list args);
-int printf_rot13(va_list args);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int _strlen(char *s);
-int *_strcpy(char *dest, char *src);
-int _strlenc(const char *s);
-int rev_string(char *s);
-int _strlenc(const char *s);
-int printf_37(void);
-int printf_char(va_list val);
-int printf_string(va_list val);
 int _putchar(char c);
+int print_int(va_list arg);
+int print_unsigned(va_list arg);
+int _printf(const char *format, ...);
+int print_char(va_list arg);
+int print_str(va_list arg);
+int print_percent(void);
+void print_binary(unsigned int n, unsigned int* printed);
+int print_unsignedToBinary(va_list arg);
+int print_oct(va_list arg);
+int print_unsignedIntToHex(unsigned int num, char _case);
+int print_hex_base(va_list arg, char _case);
+int print_hex(va_list arg);
+int print_HEX(va_list arg);
 int print_STR (va_list arg);
 int _printf(const char *format, ...);
 
-typedef struct identifierStruct
-{
-	char *indentifier;
-
-	int (*printer)(va_list);
-} identifierStruct;
 
 #endif
